b1f13439aa98aa54871ad0278fae37ca9e8e1f35
Javier Junquera-Sánchez <javier@junquera.io>
2022-09-19 12:15:16 +0200

    Saving

--- tests/test_softenum.py
+++ tests/test_softenum.py
@@ -3,20 +3,21 @@ from softenum import __version__
 from softenum import Softenum
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.0'    
 
+def test_str():
 
-class A(str, Softenum):
-
-    a = "a"
-    
-
-def test():
+    class A(str, Softenum):
+        a = "a"
     
     assert A.a == A('a')
     assert A.a != A('A')
     assert A(A.a) == A.a
 
+    assert (A("cE"))
+    assert (A("Ce"))
+
+def test_novalue():
 
     try:
         class X(str, Softenum):
@@ -30,22 +31,47 @@ def test():
     except:
         assert True
 
+def test_no_super():
 
     class X(Softenum):
+
         enumerated = "jejeje"
 
-    class X(int, Softenum):
+def test_int():
+
+    class A(int, Softenum):
+
         a = 1
+    
+    assert A.a == A(1)
+    assert A(A.a) == A.a
+
+def test_noval():
+
+    class A(Softenum):
+        pass
 
     try:
         A.be
     except:
         assert True
 
+def test_badclass():
+
+    class A(int, Softenum):
+
+        a = 1
+
     try:
         A(b"\xDE\xAD\xBE\xEF")
     except:
         assert True
+
+
+def test_fuzz():
+
+    class A(str, Softenum):
+        a = "oneval"
 
     try:
         A("_x")
@@ -54,12 +80,3 @@ def test():
     except:
         assert True
 
-
-    print(A("cE"))
-    print(A("Ce"))
-
-    print(type(A.a))
-    print(type(A('a')))
-
-if __name__ == "__main__":
-    test()
